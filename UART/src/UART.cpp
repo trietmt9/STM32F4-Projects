3f354c1190e9655c76fb6e4376c2cd6c53acfb43
unknown <pc>
2023-06-18 00:30:54 +0700

    Updated UART code

--- UART/src/UART.cpp
+++ UART/src/UART.cpp
@@ -1,14 +1,38 @@
 #include <stm32f446xx.h>
 
-#define USART2EN (1U<<17UL) // write 1 to bit 17 to Enable USART2
+#define GPIOAEN (1U << 0UL)
+#define USART2EN (1U << 17UL) // write 1 to bit 17 to Enable USART2
 
 void USART2_TX_INIT(void)
 {
     /********** Config UART/USART GPIO pin **********/
-    /*Enable clock access to GPIOA*/
-    /*Config UART baudrate*/
-    
-    /*Set PA2 to alternate function mode*/
-    /*Set PA2 alternate function to UART_TX*/
+        /*Enable clock access to GPIOA*/ 
+        RCC->AHB1ENR |= GPIOAEN;
+        
+        /*Set PA2 to alternate function mode*/
+        GPIOA->MODER &=~(1U << 4UL);
+        GPIOA->MODER |= (1U << 5UL);
 
+        /*Set PA2 alternate function to UART_TX (AFL7)*/
+        GPIOA->AFR[0] |= (1U << 28UL);
+        GPIOA->AFR[0] |= (1U << 29UL);
+        GPIOA->AFR[0] |= (1U << 30UL);
+        GPIOA->AFR[0] &=~(1U << 31UL);
+        
+    /********** Config UART/USART module **********/
+        /*Config UART baudrate*/
+        RCC->APB1ENR |= USART2EN; 
+
+        /*Config the transfer direction*/
+        /*Enable UART module*/
+
+}
+static void uart_set_baudrate(USART_TypeDef *UARTx,uint32_t PeripherialCLK, uint32_t Baudrat)
+{
+    UARTx->BRR = uart_div_compute(PeripherialCLK, PeripherialCLK);
+
+}
+static uint16_t uart_div_compute(uint32_t PeripherialCLK, uint32_t Baudrate)
+{
+    return ((PeripherialCLK + (Baudrate/2U))/Baudrate); 
 }
