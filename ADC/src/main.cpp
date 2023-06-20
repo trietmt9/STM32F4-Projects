b68ef3d0b523c092d5f29b9afb2d0756e2f289a3
unknown <pc>
2023-06-20 23:09:22 +0700

    ADC single channel library

--- ADC/src/main.cpp
+++ ADC/src/main.cpp
@@ -13,7 +13,6 @@ int main()
 {   
     Serial.USART2_INIT(115200);
     PA1_ADC_INIT();
-    
 
     while(1)
     {  
@@ -22,7 +21,7 @@ int main()
         sensor_value = adc_read();
   
         sprintf(buffer, "ADC value: %hu\n",sensor_value);
-        Serial.uart_write_string(buffer);
+        // Serial.uart_write_string(buffer);
 
     }
 }
