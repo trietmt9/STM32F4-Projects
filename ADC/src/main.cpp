a1428ff8c98f80f295a3d0843ae4c089110a339e
unknown <pc>
2023-06-20 23:00:28 +0700

    ADC Library

--- ADC/src/main.cpp
+++ ADC/src/main.cpp
@@ -1,23 +1,28 @@
 #include <stm32f446xx.h>
+#include <iostream>
 #include <stdint.h>
 #include <stdio.h>
 #include <UART.h>
 #include <ADC.h>
+using namespace std;
 
 uint32_t sensor_value; 
 UART Serial;
 
 int main()
 {   
-    Serial.USART2_INIT(9600);
+    Serial.USART2_INIT(115200);
     PA1_ADC_INIT();
-    adc_conversion();
+    
 
     while(1)
-    {   
+    {  
+        char buffer[16]; 
+        adc_conversion();
         sensor_value = adc_read();
-        Serial.uart_write_string("Sensor value: ");
-        Serial.uart_write(sensor_value);
+  
+        sprintf(buffer, "ADC value: %hu\n",sensor_value);
+        Serial.uart_write_string(buffer);
 
     }
 }
